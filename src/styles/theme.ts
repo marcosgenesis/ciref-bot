650215aad750633e167d32e75ef4cbcd18ab59ab
Marcos Gênesis <49327985+marcosgenesis@users.noreply.github.com>
2022-04-24 22:48:20 -0300

    Initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+import {extendTheme} from '@chakra-ui/react'
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      900: '#0F0F0F',
+      800: '#151718',
+      700: '#242627',
+      600: '#323337',
+      500: '#424448',
+      400: '#696C74',
+      300: '#767981',
+      200: '#90939B',
+      100: '#A9ADB2',
+      50: '#C4C6CA',
+    },
+    green: {
+      500: '#37cb95',
+      300: '#c3efdf',
+      50: '#e1f7ef',
+    },
+    red: {
+      500: '#de3c63',
+      300: '#f5c5d0',
+      50: '#fcecef',
+    },
+    yellow: {
+      500: '#eea620',
+      300: '#fae4bc',
+      50: '#f5efdf',
+    },
+})
