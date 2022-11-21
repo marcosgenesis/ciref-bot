9b3b62d9dc643a10290886f7b591d0b35695f871
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-11-21 16:58:56 -0300

    fix: change prisma lib folder

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,6 @@ import {PrismaClient} from '@prisma/client'
 
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
