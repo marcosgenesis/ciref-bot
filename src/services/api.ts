5371edf21eedbea87b58ea0ffd5077b859357ac3
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-11-22 22:57:50 -0300

    fix: backend api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 export const apiGithub = axios.create({
   baseURL: 'https://api.github.com/',
-  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`},
+  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}` },
 });
 
 export const backendApi = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: process.env.BACKEND_API,
 });
